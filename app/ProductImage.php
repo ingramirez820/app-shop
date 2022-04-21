af72d45597ed9616161328470a1c6ff5c9a05bca
El HR <ingramirez820@gmail.com>
2022-04-21 01:33:46 -0500

    Relacion de modelos, controlador y uso de tinker

--- app/ProductImage.php
+++ app/ProductImage.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    //
+    //$productImage->array_product
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
